927aa288227741e757e412de6f4984feb58bbcf1
Aks070 <113827304+Aks070@users.noreply.github.com>
2024-01-29 20:47:55 +0100

    1

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -18,9 +20,20 @@ public class StudentService {
         this.studentRepository = studentRepository;
     }
 
-
     @GetMapping
     public List<Student> getStudents(){
         return studentRepository.findAll();
+    }
+
+    @PostMapping
+    public void addNewStudent(Student student) throws Exception {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+
+        if (studentOptional.isPresent()){
+            throw new Exception("Email already exists!");
+        }
+        studentRepository.save(student);
+
+        System.out.println(student);
     }
 }
